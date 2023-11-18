434dfd629e314cea729ab9c3bcc43d7f5d4de18e
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-11-17 16:23:13 -0800

    Make local storage work execute on client only

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,12 +2,20 @@
 import React from 'react';
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(0);
 
+  // on client, on page load, get count from local storage
+  React.useEffect(() => {
+    const savedCount = window.localStorage.getItem('saved-count');
+
+    if(savedCount === null) {
+      return;
+    }
+    
+    setCount(Number(savedCount));
+  }, []);
+
+    // whenever count is changed, update local storage accordingly
   React.useEffect(() => {
     window.localStorage.setItem('saved-count', count);
   }, [count]);
@@ -15,11 +23,16 @@ function Counter() {
   return (
     <button
       className="count-btn"
-      onClick={() => setCount(count + 1)}
+      onClick={() => {
+          const nextCount = count + 1;
+          setCount(nextCount);
+        }
+      }  
     >
       Count: {count}
     </button>
   );
+
 }
 
 export default Counter;
